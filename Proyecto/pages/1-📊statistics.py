0e9a5f846ac89ee1ceee0e0b352310073b0e7a2b
rentsmart-ai <rentsmart2024@gmail.com>
2024-10-17 14:53:59 -0500

    Update 1-📊statistics.py

--- Proyecto/pages/1-📊statistics.py
+++ Proyecto/pages/1-📊statistics.py
@@ -27,7 +27,7 @@ st.write("---")
 
 #_____________________________________CONEXION BASE DE DATOS_________________________________________#
 # Cargar las credenciales desde Streamlit secrets
-conn= st.secrets[postgres]
+conn= st.secrets['postgres']
 
 # Crear la cadena de conexión
 connection_string = f"{conn['dialect']}://{conn['username']}:{conn['password']}@{conn['host']}:{conn['port']}/{conn['database']}"
